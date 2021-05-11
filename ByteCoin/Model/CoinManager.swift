8ca2c922d85d12f1c1251a2d035932ac0f449f53
Gabriela Olson <gabriela.olson@icloud.com>
2021-05-10 21:26:28 -0300

    update project

---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,78 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Angela Yu on 11/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+protocol CoinManagerDelegate {
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+struct CoinManager {
+    
+    var delegate: CoinManagerDelegate?
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "your_api_key"
+    
+    
+    
+    
+
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(for currency: String){
+        let urlString = "\(baseURL)/\(currency)?apiKey=\(apiKey)"
+        
+        if let url = URL(string: urlString){
+            
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url){(data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let bitCoinPrice = self.parseJSON(safeData){
+                        let priceString = String(format: "%.2f", bitCoinPrice)
+                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
+                    }
+                }
+//                print(dataAsString)
+                
+                
+            }
+            task.resume()
+        }
+        
+    }
+    
+    func parseJSON(_ data: Data) -> Double? {
+    let decoder = JSONDecoder()
+    do{
+        let decodedData = try decoder.decode(CoinData.self, from: data)
+        let lastPrice = decodedData.rate
+        print(lastPrice)
+        return lastPrice
+    } catch {
+        print(error)
+        return nil
+    }
+    }
+    
+    
+
+    }
+//    func fetchRate(for ){
+//        getCoinPrice(for: <#T##String#>)
+//        let urlString = "\(baseURL)&q=\(currencyArray[row])"
+//    }
+//
+
+    
+
---
+++ ByteCoin/Model/CoinModel.swift
@@ -0,0 +1,11 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by Gabriela on 10/05/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
